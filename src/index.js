46ee2e4f4546a88de92d7c83e34b5bbc3ce01227
Zhenya84 <55429535+Zhenya84@users.noreply.github.com>
2020-09-29 12:40:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array.length !== 0) {
+        let result = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (result > array[i]) {
+        result = array[i]}
+        }
+        return result}
+    else return 0
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array.length !== 0) {
+        let result = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (result < array[i]) {
+        result = array[i]}
+        }
+        return result}
+    else return 0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array.length !== 0) {
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += array[i]}
+    return result/(array.length)}
+  else return 0
 }
