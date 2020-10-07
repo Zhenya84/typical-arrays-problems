87b53f85c4b1e5a7519f2eda4b60c9a999c27fbf
Zhenya84 <55429535+Zhenya84@users.noreply.github.com>
2020-10-07 22:30:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 
 exports.min = function min (array) {
-    if (array.length !== 0) {
-        let result = array[0];
-        for (let i = 0; i < array.length; i++) {
-            if (result > array[i]) {
-        result = array[i]}
-        }
-        return result}
-    else return 0
+        if ((array == undefined) || (array.length == 0)) return 0
+        else {
+            let result = array[0];
+            for (let i = 0; i < array.length; i++) {
+                if (result > array[i]) {
+                    result = array[i]}
+            }
+            return result}
 }
 
 exports.max = function max (array) {
-    if (array.length !== 0) {
-        let result = array[0];
-        for (let i = 0; i < array.length; i++) {
-            if (result < array[i]) {
-        result = array[i]}
-        }
-        return result}
-    else return 0
+        if ((array == undefined) || (array.length == 0)) return 0
+        else {
+            let result = array[0];
+            for (let i = 0; i < array.length; i++) {
+                if (result < array[i]) {
+                    result = array[i]}
+            }
+            return result}
 }
 
 exports.avg = function avg (array) {
-  if (array.length !== 0) {
-    let result = 0;
-    for (let i = 0; i < array.length; i++) {
-        result += array[i]}
-    return result/(array.length)}
-  else return 0
+  if ((array == undefined) || (array.length == 0)) return 0
+        else {
+            let result = 0;
+            for (let i = 0; i < array.length; i++) {
+                result += array[i]}
+            return result/(array.length)}
 }
